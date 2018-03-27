1796a4fb432efe9d09c09e6f6ca34640fa8db945
WINDEV1802EVAL\User <andrew.clawson@clawsonanalytics.com>
2018-03-26 21:32:29 -0600

    Initial commit.

---
+++ Hvac/Hvac/Hvac/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Hvac.Models;
+
+namespace Hvac.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

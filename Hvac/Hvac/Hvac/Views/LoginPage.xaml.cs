71737a7c6b979ca88902cfe83e8409391889eb66
WINDEV1802EVAL\User <andrew.clawson@clawsonanalytics.com>
2018-03-31 23:23:47 -0600

    add home master / detail views.

--- Hvac/Hvac/Hvac/Views/LoginPage.xaml.cs
+++ Hvac/Hvac/Hvac/Views/LoginPage.xaml.cs
@@ -16,5 +16,10 @@ namespace Hvac.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        async private void LoginClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomeMasterDetailPage());
+        }
+    }
 }

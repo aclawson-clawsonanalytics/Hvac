fec3bdacf6b031e90ad8221e55afccce8368795e
ANDREWCLAWSA011\andrewclawson <aclawson@clawsonanalytics.com>
2018-04-13 19:38:43 -0600

    Add new view model files.

---
+++ Hvac/Hvac/Hvac/Views/ReferenceDocumentMasterDetailPageMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hvac.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReferenceDocumentMasterDetailPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public ReferenceDocumentMasterDetailPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new ReferenceDocumentMasterDetailPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class ReferenceDocumentMasterDetailPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<ReferenceDocumentMasterDetailPageMenuItem> MenuItems { get; set; }
+            
+            public ReferenceDocumentMasterDetailPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<ReferenceDocumentMasterDetailPageMenuItem>(new[]
+                {
+                    new ReferenceDocumentMasterDetailPageMenuItem { Id = 0, Title = "Page 1" },
+                    new ReferenceDocumentMasterDetailPageMenuItem { Id = 1, Title = "Page 2" },
+                    new ReferenceDocumentMasterDetailPageMenuItem { Id = 2, Title = "Page 3" },
+                    new ReferenceDocumentMasterDetailPageMenuItem { Id = 3, Title = "Page 4" },
+                    new ReferenceDocumentMasterDetailPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ Hvac/Hvac/Hvac/Views/ReferenceDocumentMasterDetailPageMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hvac.Views
+{
+
+    public class ReferenceDocumentMasterDetailPageMenuItem
+    {
+        public ReferenceDocumentMasterDetailPageMenuItem()
+        {
+            TargetType = typeof(ReferenceDocumentMasterDetailPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
